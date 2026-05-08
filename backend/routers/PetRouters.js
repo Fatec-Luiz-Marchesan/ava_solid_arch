d0fab5a6f6bcd3580c1003a59a2195d4ce9b524a
ferreirajoao-fatec <joao.ferreira11>
2026-05-07 23:00:58 -0300

    Estruturação das Rotas (PetRoutes.js)

--- backend/routers/PetRouters.js
+++ backend/routers/PetRouters.js
@@ -2,19 +2,19 @@ const router = require('express').Router()
 
 const PetController = require('../controllers/PetController')
 
-const { verifyToken } = require('../helpers/verify-token')
+const  verifyToken  = require('../helpers/verify-token')
 const {imageUpload} = require('../helpers/image-upload')
 
-router.post('/create', verifyToken, imageUpload.array('images', 5), PetController.createPet)
-router.get('/all', PetController.getAll)
+router.post('/create', verifyToken, imageUpload.array('images', 5), PetController.create)
+router.get('/getall', PetController.getAll)
 router.get('/getAllUserPets', verifyToken, PetController.getAllUserPets)
 router.get('/getAllUserAdoptions', verifyToken, PetController.getAllUserAdoptions)
 
-router.patch('/schedule/:id', verifyToken, PetController.schedule)
-router.patch('/concludeAdoption/:id', verifyToken, PetController.concludeAdoption)
+router.post('/schedule/:id', verifyToken, PetController.schedule)
+router.post('/concludeAdoption/:id', verifyToken, PetController.concludeAdoption)
 
 router.patch('/:id', verifyToken, imageUpload.array('images', 5), PetController.updatePet)
-router.delete('/:id', verifyToken, PetController.deletePet)
+router.delete('/:id', verifyToken, PetController.removePet)
 router.get('/:id', PetController.getPetById)
 
 
